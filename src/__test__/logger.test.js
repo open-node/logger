23fe5cdf120a4627bb96aa2b152d63b13ed93d7d
Redstone Zhao <13740080@qq.com>
2019-11-27 15:53:13 +0800

    Completed

---
+++ src/__test__/logger.test.js
@@ -0,0 +1,140 @@
+const fs = require("fs");
+const path = require("path");
+const Logger = require("../../");
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const infoLogPath = path.join(__dirname, "log");
+const errorLogPath = path.join(__dirname, "log");
+jest.mock("fs");
+
+describe("Logger module", () => {
+  it("instance method", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    expect(typeof logger.info).toBe("function");
+    expect(typeof logger.error).toBe("function");
+    expect(typeof logger.logger).toBe("function");
+  });
+
+  it("info method", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    logger.info("hello");
+    expect(fs.appendFileSync.mock.calls.length).toBe(1);
+    const [file, line] = fs.appendFileSync.mock.calls[0];
+    expect(file.slice(0, infoLogPath.length)).toBe(infoLogPath);
+    expect(line).toMatch("hello");
+  });
+
+  it("info method, has extra", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    logger.info("hello", "world");
+    expect(fs.appendFileSync.mock.calls.length).toBe(2);
+    const [file, line] = fs.appendFileSync.mock.calls[1];
+    expect(file.slice(0, infoLogPath.length)).toBe(infoLogPath);
+    expect(line).toMatch("hello");
+    expect(line).toMatch("world");
+  });
+
+  it("error method", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    logger.error(Error("hello"), "world");
+    expect(fs.appendFileSync.mock.calls.length).toBe(3);
+    const [file, line] = fs.appendFileSync.mock.calls[2];
+    expect(file.slice(0, errorLogPath.length)).toBe(errorLogPath);
+    expect(line).toMatch("hello");
+    expect(line).toMatch("world");
+  });
+
+  it("error method, has extra", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    logger.error(Error("nihao"));
+    expect(fs.appendFileSync.mock.calls.length).toBe(4);
+    const [file, line] = fs.appendFileSync.mock.calls[3];
+    expect(file).toMatch("unknown");
+    expect(file.slice(0, errorLogPath.length)).toBe(errorLogPath);
+    expect(line).not.toMatch("world");
+  });
+
+  it("error method, no stack, has extra", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    logger.error({ code: "errorcode", message: "nihao" });
+    expect(fs.appendFileSync.mock.calls.length).toBe(5);
+    const [file, line] = fs.appendFileSync.mock.calls[4];
+    expect(file.slice(0, errorLogPath.length)).toBe(errorLogPath);
+    expect(file).toMatch("errorcode");
+    expect(line).not.toMatch("world");
+  });
+
+  it("logger sync method, fn exec success", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    const fn = jest.fn();
+    fn.mockReturnValueOnce(10);
+    const fnLog = logger.logger(fn, "testing", false);
+
+    const res = fnLog(1, 2, 3, 4);
+
+    expect(res).toBe(10);
+    expect(fn.mock.calls.length).toBe(1);
+    expect(fn.mock.calls[0]).toEqual([1, 2, 3, 4]);
+
+    expect(fs.appendFileSync.mock.calls.length).toBe(7);
+  });
+
+  it("logger sync method, fn exec faild", () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    const calls = [];
+    const fn = (...args) => {
+      calls.push(args);
+      throw Error("wrong");
+    };
+    const fnLog = logger.logger(fn, "testing", false);
+
+    expect(() => fnLog(1, 2, 3, 4)).toThrow("wrong");
+    expect(calls.length).toBe(1);
+    expect(calls[0]).toEqual([1, 2, 3, 4]);
+
+    expect(fs.appendFileSync.mock.calls.length).toBe(9);
+  });
+
+  it("logger async method, fn exec success", async () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    const calls = [];
+    const ret = "I am function return value";
+    const fn = async (...args) => {
+      calls.push(args);
+      return new Promise(resolve => {
+        setTimeout(resolve.bind(null, ret), 50);
+      });
+    };
+    const fnLog = logger.logger(fn, "testing", true);
+
+    const res = await fnLog(1, 2, 3, 4);
+
+    expect(res).toBe(ret);
+    expect(calls.length).toBe(1);
+    expect(calls[0]).toEqual([1, 2, 3, 4]);
+
+    expect(fs.appendFileSync.mock.calls.length).toBe(11);
+    expect(fs.appendFileSync.mock.calls[10][1]).toMatch(ret);
+  });
+
+  it("logger async method, fn exec faild", async () => {
+    const logger = new Logger({ infoLogPath, errorLogPath });
+    const calls = [];
+    const fn = async (...args) => {
+      calls.push(args);
+      return new Promise((resolve, reject) => {
+        setTimeout(reject.bind(null, Error("wrong")), 50);
+      });
+    };
+    const fnLog = logger.logger(fn, "testing", true);
+
+    expect(fnLog(3, 2, 3, 4)).rejects.toThrow("wrong");
+    await sleep(51);
+    expect(calls.length).toBe(1);
+    expect(calls[0]).toEqual([3, 2, 3, 4]);
+
+    expect(fs.appendFileSync.mock.calls.length).toBe(13);
+    expect(fs.appendFileSync.mock.calls[12][1]).toMatch("wrong");
+  });
+});
---
+++ src/logger.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+const uuidv4 = require("uuid/v4");
+
+const now = () => new Date().toISOString();
+
+/**
+ * @param {Object} config 配置信息，errorLogPath, infoLogPath, 必须包含
+ * @class
+ * @return {Logger} Instance
+ */
+function Logger({ errorLogPath, infoLogPath }) {
+  /**
+   * 记录错误信息
+   * @memberof Logger
+   * @instance
+   * @param {error} e error对象
+   * @param {any} [extra] 额外要记录的信息
+   *
+   * @return {void}
+   */
+  const error = (e, extra) => {
+    const code = e.code || "unknown";
+    const file = `${errorLogPath}/${code}.err`;
+    const content = [now(), e.message];
+    if (extra != null) content.push(JSON.stringify(extra));
+    if (e.stack) content.push(JSON.stringify(e.stack));
+    fs.appendFileSync(file, `${content.join("\t")}\n`);
+  };
+
+  /**
+   * 记录普通信息
+   * @memberof Logger
+   * @instance
+   * @param {string} message 要记录的信息
+   * @param {any} [extra] 额外要记录的信息
+   *
+   * @return {void}
+   */
+  const info = (message, extra) => {
+    const today = now().split("T")[0];
+    const file = `${infoLogPath}/${today}.log`;
+    const content = [now(), message];
+    if (extra != null) content.push(JSON.stringify(extra));
+    fs.appendFileSync(file, `${content.join("\t")}\n`);
+  };
+
+  /**
+   * 将普通函数加工成带有日志记录功能的函数
+   * @memberof Logger
+   * @instance
+   * @param {function} fn 要加工的函数
+   * @param {string} name 名称、归类
+   * @param {boolean} [isAsync=true] 是否是异步函数
+   * @param {function} [transform] 返回值记录的tans 函数
+   * @param {function} [errorHandler] 错误信息处理函数
+   *
+   * @return {function}
+   */
+  const logger = (
+    fn,
+    name,
+    isAsync = true,
+    transform = x => x,
+    errorHandler = e => e.message
+  ) => {
+    if (isAsync) {
+      return async (...args) => {
+        const callId = uuidv4();
+        try {
+          info(`Begin: ${name}\t${callId}\t${JSON.stringify(args)}`);
+          const res = await fn(...args);
+          info(
+            `Completed: ${name}\t${callId}\t${JSON.stringify(transform(res))}`
+          );
+          return res;
+        } catch (e) {
+          info(`Error: ${name}\t${callId}\t${errorHandler(e)}`, e.stack);
+          throw e;
+        }
+      };
+    }
+    return (...args) => {
+      const callId = uuidv4();
+      try {
+        info(`Begin: ${name}\t${callId}\t${JSON.stringify(args)}`);
+        const res = fn(...args);
+        info(`Completed: ${name}\t${callId}\t${JSON.stringify(res)}`);
+        return res;
+      } catch (e) {
+        info(`Error: ${name}\t${callId}\t${e.message}`, e.stack);
+        throw e;
+      }
+    };
+  };
+
+  return { error, info, logger };
+}
+
+module.exports = Logger;
